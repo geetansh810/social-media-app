70ec01ecb31f7b212df07a22c14aca993f0caae1
Geetansh Agrawal <66216511+geetansh810@users.noreply.github.com>
2022-06-10 03:05:47 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDqiG0RCMB9Y7KW0BhSrtr27d427M-tLqc",
+    authDomain: "socialmediaapp-3b9c8.firebaseapp.com",
+    databaseURL: "https://socialmediaapp-3b9c8-default-rtdb.firebaseio.com",
+    projectId: "socialmediaapp-3b9c8",
+    storageBucket: "socialmediaapp-3b9c8.appspot.com",
+    messagingSenderId: "42050856229",
+    appId: "1:42050856229:web:57cf5764dff4860dad66cc",
+    measurementId: "G-H2MKYRQJHC"
+  }
+
 };
